b9c904557b5d9b62f438554bcb1f1870ee718c7e
rajayush01 <119657235+rajayush01@users.noreply.github.com>
2026-08-13 18:19:31 +0530

    changes

--- src/pages/Journal.tsx
+++ src/pages/Journal.tsx
@@ -529,29 +529,29 @@ const STORIES: Story[] = [
   },
 ];
 
-const heroContainer = {
-  hidden: {},
-  visible: {
-    transition: {
-      staggerChildren: 0.12,
-    },
-  },
-};
+// const heroContainer = {
+//   hidden: {},
+//   visible: {
+//     transition: {
+//       staggerChildren: 0.12,
+//     },
+//   },
+// };
 
-const heroItem = {
-  hidden: {
-    opacity: 0,
-    y: 25,
-  },
-  visible: {
-    opacity: 1,
-    y: 0,
-    transition: {
-      duration: 0.9,
-      ease: [0.22, 1, 0.36, 1] as const,
-    },
-  },
-};
+// const heroItem = {
+//   hidden: {
+//     opacity: 0,
+//     y: 25,
+//   },
+//   visible: {
+//     opacity: 1,
+//     y: 0,
+//     transition: {
+//       duration: 0.9,
+//       ease: [0.22, 1, 0.36, 1] as const,
+//     },
+//   },
+// };
 
 const storyVariants = {
   hidden: {
